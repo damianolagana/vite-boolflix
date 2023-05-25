07d4d48fc496657b38f52c4111dca6fbc2ce5cf8
damianolagana <damianolagana@gmail.com>
2023-05-25 18:25:42 +0200

    print movies details

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,6 @@
 <script>
 import { store } from '../data/store';
+import axios from 'axios'
 
 export default {
     name: "AppHeader",
@@ -12,6 +13,16 @@ export default {
     methods: {
         findMovies() {
             console.log(this.searchedMovie)
+            this.store.fullAPI += this.store.query + this.searchedMovie;
+            console.log(this.store.fullAPI)
+
+            axios.get(this.store.fullAPI).then(answer => {
+                console.log(answer)
+                this.store.movies = answer.data.results
+                console.log(store.movies)
+
+            })
+
             this.searchedMovie = ""
         }
     }
@@ -27,6 +38,28 @@ export default {
         <input type="text" placeholder="Cerca" v-model="searchedMovie">
         <button @click="findMovies()">Search</button>
     </div>
+    <div id="moviesBox">
+        <div v-for="movie in store.movies">
+            <h2>Movie</h2>
+            <ul>
+                <li>{{ movie.title }}</li>
+                <li>{{ movie.original_title }}</li>
+                <li>{{ movie.original_language }}</li>
+                <li>{{ movie.vote_count }}</li>
+            </ul>
+        </div>
+
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#moviesBox {
+    display: flex;
+    flex-wrap: wrap;
+    width: 1200px;
+}
+
+#moviesBox>div {
+    width: calc(100%/5);
+}
+</style>
