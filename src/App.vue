a26f6076766305e14cea1b29782ccac957a63ed7
damianolagana <damianolagana@gmail.com>
2023-05-25 16:32:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+
+        }
+    }
+}
+
+
+
+</script>
+
+<template>
+    <div>
+        <input type="text" placeholder="Cerca">
+        <button>Search</button>
+    </div>
+</template>
+
+<style scoped></style>
